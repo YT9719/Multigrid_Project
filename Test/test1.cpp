a0362295702114f327c2363756246c6edbe81ccd
Shuai Che <shuaiche@umich.edu>
2021-11-18 17:40:30 -0500

    Complete the first test file.

--- Test/test1.cpp
+++ Test/test1.cpp
@@ -1,7 +1,7 @@
 /*
 First simple test
 1D homogeneous problem Au = 0
-Finest grid n = 64 points
+Finest grid n = 64 intervals (65 points/nodes)
 Two level grid 
 The initial guess is two sine waves (low frequency error + high frequency error)
 v_0(x) = 0.5*(sin(3*M_PI*x/n)+sin(10*M_PI*x/n))
@@ -16,50 +16,93 @@ v_0(x) = 0.5*(sin(3*M_PI*x/n)+sin(10*M_PI*x/n))
 using namespace std;
 
 int main(){
-    int n = 8;
-    int l1 = n; // fine grid
-    int l2 = (n+1)/2; // coarse grid
+    int n = 64;
+    int l1 = n+1; // fine grid
+    int l2 = (l1+1)/2; // coarse grid
 
-    // define the coefficient matrix A
-    double **A = allocate_mat(l1, l1);
-    initialize_mat(A, l1, l1);
+    // define the coefficient matrix A1 for the fine grid
+    double **A1 = allocate_mat(l1, l1);
+    initialize_mat(A1, l1, l1);
     for(int i = 0; i < l1; i++){
         if(i == 0){
-            A[i][i] = 1; A[i][i+1] = 0.5;
+            A1[i][i] = 1; A1[i][i+1] = 0.5;
         }
         else if(i == l1 - 1){
-            A[i][i] = 1; A[i][i-1] = 0.5; 
+            A1[i][i] = 1; A1[i][i-1] = 0.5; 
         }
         else{
-            A[i][i] = 1; A[i][i+1] = 0.5; A[i][i-1] = 0.5;
+            A1[i][i] = 1; A1[i][i+1] = 0.5; A1[i][i-1] = 0.5;
         }
     }
 
-    // define the right hand side vector
-    double *f = new double[l1];
+    // define the right hand side vector f1 for the fine grid
+    double *f1 = new double[l1];
+    initialize_mat(f1, l1);
+
+    // define the initial guess v1 for the fine grid
+    double *v1 = new double[l1];
     for(int i = 0; i < l1; i++){
-        f[i] = 0;
+        v1[i] = 0.5*(sin(3*M_PI*i/(l1-1))+sin(10*M_PI*i/(l1+1)));
     }
 
-    // define the initial guess
-    double *v = new double[l1];
-    for(int i = 0; i < l1; i++){
-        v[i] = 0.5*(sin(3*M_PI*i/(l1-1))+sin(10*M_PI*i/(l1+1)));
-    }
-
-    // compute the residual
-    double *residual = new double[l1];
-    residual = getResidual(A, f, v, l1);
-
+    // compute the residual r1 for the fine grid
+    double *r1 = new double[l1];
+    r1 = getResidual(A1, f1, v1, l1);
+    
     //print maximum norm of the residual
-    cout<<norm_max(residual, l1)<<endl;
+    cout<<"Maximum norm of residual: ";
+    cout<<norm_max(r1, l1)<<endl;
 
     // first relaxation/smoothing
-    v = GS(l1, 1, A, f, v);
-    residual = getResidual(A, f, v, l1);
+    v1 = GS(l1, 1, A1, f1, v1);
+    r1 = getResidual(A1, f1, v1, l1);
 
     //print maximum norm of the residual
-    cout<<norm_max(residual, l1)<<endl;
+    cout<<"Maximum norm of residual: ";
+    cout<<norm_max(r1, l1)<<endl;
+
+    // restriction
+    // compute the residual r1 for the coarse grid
+    double *r2 = new double[l2];
+    r2 = restrict(r1, l1);
+
+    // define the coefficient matrix A1 for the coarse grid
+    double **A2 = allocate_mat(l2, l2);
+    initialize_mat(A2, l2, l2);
+    for(int i = 0; i < l2; i++){
+        if(i == 0){
+            A2[i][i] = 1; A2[i][i+1] = 0.5;
+        }
+        else if(i == l1 - 1){
+            A2[i][i] = 1; A2[i][i-1] = 0.5; 
+        }
+        else{
+            A2[i][i] = 1; A2[i][i+1] = 0.5; A2[i][i-1] = 0.5;
+        }
+    }
+
+    // define the error e2 on the coarse grid
+    double *e2 = new double[l2];
+    initialize_mat(e2, l2);
+
+    // second relaxation/smoothing
+    e2 = GS(l2, 1, A2, r2, e2);
+
+    // prolongation
+    // compute the error e1 for the fine grid
+    double *e1 = new double[l1];
+    e1 = prolong(e2, l2);
+
+    // correct the solution v1 for the fine grid
+    v1 = add_vv(v1, e1, l1);
+
+    // third relaxation/smoothing
+    v1 = GS(l1, 1, A1, f1, v1);
+    r1 = getResidual(A1, f1, v1, l1);
+
+    //print maximum norm of the residual
+    cout<<"Maximum norm of residual: ";
+    cout<<norm_max(r1, l1)<<endl;
 
     return 1;
 }
